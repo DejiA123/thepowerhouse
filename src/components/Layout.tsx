4e06904ee57a292695b6652aa56ed796684e5d65
Deji Augustine <dejiaugustine@gmail.com>
2026-01-18 19:43:01 +0000

    Matched Layout spacer to new Absolute Zero nav bar height of 45px

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -195,7 +195,7 @@ const Layout = ({ children }: LayoutProps) => {
       >
         <div className={cn(
           "min-h-full",
-          showChrome && location.pathname !== '/group-chats' ? "pb-[50px]" : (location.pathname === '/group-chats' ? "h-full" : "")
+          showChrome && location.pathname !== '/group-chats' ? "pb-[45px]" : (location.pathname === '/group-chats' ? "h-full" : "")
         )}>
           {children}
         </div>
