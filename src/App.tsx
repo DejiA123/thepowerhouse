165441f1b1365472ff61b17047ab04c3be3fd0d8
Deji Augustine <dejiaugustine@gmail.com>
2026-02-27 00:59:26 +0000

    feat(ui): globally disable all toast notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 // App.tsx - Fixed BrowserRouter caching issue
 import React, { useEffect } from "react";
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ScrollToTop from "@/components/ScrollToTop";
@@ -361,8 +359,6 @@ const App = () => {
                   <CallProvider>
                     <CallManager />
                     <AppRoutes />
-                    <Toaster />
-                    <Sonner />
                   </CallProvider>
                 </NotificationProvider>
               </TooltipProvider>
